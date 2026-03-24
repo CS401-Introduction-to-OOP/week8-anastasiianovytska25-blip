82cc6cbbfebedd25a30d80c76848d21e77dcf27b
nastya <anastasiia.novytska.25@kse.org.ua>
2026-03-24 18:48:52 +0200

    Stage 1

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public abstract class DeliveryItem
+{
+    public string TrackingNumber { get ; }
+    public double Weight { get ; }
+    public DeliveryItem (string trackingNumber, double weight)
+    {
+        TrackingNumber = trackingNumber ;
+        Weight = weight ;
+    }
+    public abstract double CalculateCost () ;
+    public virtual void PrintInfo ()
+    {
+        Console.WriteLine($"Track : {TrackingNumber} | Weight : { Weight }") ;
+    }
+}
 
-Console.WriteLine("Hello, World!");
+public  class Letter(string TrackingNumber, double Weight) : DeliveryItem(TrackingNumber, Weight)
+{
+    public override double CalculateCost()
+    {
+        return 15 + (Weight * 10);
+    }
+}
+
+public  class Parcel(string TrackingNumber, double Weight, string Dimensions ) : DeliveryItem(TrackingNumber, Weight)
+{
+    public string Dimensions { get; } = Dimensions;
+    public override double CalculateCost()
+    {
+        return 50 + (Weight * 25);
+    }
+    
+    public override void PrintInfo()
+    {
+        base.PrintInfo();
+        Console.WriteLine($" Dimentsions : {Dimensions}");
+    }
+}
